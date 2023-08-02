93664def88a5f61c18755750dc46f2ec941b43b0
Fanwang Meng <fwmeng88@gmail.com>
2023-08-02 13:32:42 -0400

    Add documentation for base class

--- hardware_pydantic/base.py
+++ hardware_pydantic/base.py
@@ -1,8 +1,8 @@
 from __future__ import annotations
 
 from typing import Any, Literal, Type
-
-from N2G import drawio_diagram  # only used for drawing instruction DAG
+# only used for drawing instruction DAG
+from N2G import drawio_diagram
 from pydantic import BaseModel, Field
 
 from .utils import str_uuid
@@ -10,22 +10,26 @@ from .utils import str_uuid
 DEVICE_ACTION_METHOD_PREFIX = "action__"
 DEVICE_ACTION_METHOD_ACTOR_TYPE = Literal['pre', 'post', 'proj']
 
+"""Base classes for hardware units."""
+
 
 class Individual(BaseModel):
-    """ a thing with an identifier """
+    """A thing with an identifier."""
 
     identifier: str = Field(default_factory=str_uuid)
 
     def __hash__(self):
+        """Hash by identifier."""
         return hash(self.identifier)
 
     def __eq__(self, other: Individual):
+        """Compare by identifier."""
         return self.identifier == other.identifier
 
 
 class LabObject(Individual):
     """
-    a physical object in a lab that
+    A lab object is a physical object in a lab that
     1. is not a chemical designed to participate in reactions
     2. has constant, physically defined 3D boundary
     3. has a (non empty) set of measurable qualities that need to be tracked, this set is the state of this `LabObject`
@@ -35,6 +39,7 @@ class LabObject(Individual):
 
     @property
     def state(self) -> dict:
+        """The current state of this lab object as a dictionary."""
         d = {}
         for k, v in self.__dict__.items():
             if k.startswith("layout"):
@@ -43,23 +48,27 @@ class LabObject(Individual):
         return d
 
     def validate_state(self, state: dict) -> bool:
+        """Validate a state dictionary."""
         pass
 
     def validate_current_state(self) -> bool:
+        """Validate the current state of this lab object."""
         return self.validate_state(self.state)
 
 
 class PreActError(Exception):
+    """Error raised when pre-actor method fails."""
     pass
 
 
 class PostActError(Exception):
+    """Error raised when post-actor method fails."""
     pass
 
 
 class Device(LabObject):
     """
-    a `LabObject` that
+    A `LabObject` is a physical object in a lab that
     1. can receive instructions
     2. can change its state and other lab objects' states using its action methods,
     3. cannot change another device's state # TODO does this actually matter?
@@ -67,7 +76,7 @@ class Device(LabObject):
 
     @property
     def action_names(self) -> list[str]:
-        """ a sorted list of the names of all defined actions """
+        """A sorted list of the names of all defined actions."""
         names = sorted(
             {k[len(DEVICE_ACTION_METHOD_PREFIX):] for k in dir(self) if k.startswith(DEVICE_ACTION_METHOD_PREFIX)}
         )
@@ -79,9 +88,15 @@ class Device(LabObject):
             **kwargs
     ) -> tuple[list[LabObject], float] | None:
         """
-        ACTION: dummy
-        DESCRIPTION: wait for certain amount of time
-        PARAMS:
+        A dummy action method that does nothing,
+        but can be used as a template for other action methods.
+
+        Parameters
+        ----------
+        actor_type : DEVICE_ACTION_METHOD_ACTOR_TYPE
+            The type of actor method to call, one of 'pre', 'post', 'proj'. Default is 'pre'.
+        **kwargs : dict
+            The parameters for the action method.
         """
         assert 'actor_type' not in kwargs
         if actor_type == 'pre':
@@ -106,21 +121,61 @@ class Device(LabObject):
             actor_type: Literal['pre', 'post', 'proj'] = 'pre',
             action_parameters: dict[str, Any] = None,
     ):
+        """
+        Get the action method of this device by name and call it.
+
+        Parameters
+        ----------
+        action_name : str
+            The name of the action method to call. Default is 'dummy'.
+        actor_type : Literal['pre', 'post', 'proj']
+            The type of actor method to call, one of 'pre', 'post', 'proj'. Default is 'pre'.
+        action_parameters : dict[str, Any]
+            The parameters for the action method. Default is None.
+        """
         assert action_name in self.action_names, f"{action_name} not in {self.action_names}"
         if action_parameters is None:
             action_parameters = dict()
         method_name = DEVICE_ACTION_METHOD_PREFIX + action_name
+
         return getattr(self, method_name)(actor_type=actor_type, **action_parameters)
 
-    def act_by_instruction(self, i: Instruction, actor_type: DEVICE_ACTION_METHOD_ACTOR_TYPE):
-        """ perform action with an instruction """
-        assert i.device == self
-        return self.act(action_name=i.action_name, action_parameters=i.action_parameters, actor_type=actor_type)
+    def act_by_instruction(self, instruct: Instruction, actor_type: DEVICE_ACTION_METHOD_ACTOR_TYPE):
+        """Perform action with an instruction.
+
+        Parameters
+        ----------
+        instruct : Instruction
+            The instruction to perform.
+        actor_type : DEVICE_ACTION_METHOD_ACTOR_TYPE
+            The type of actor method to call, one of 'pre', 'post', 'proj'.
+
+        Returns
+        -------
+        tuple[list[LabObject], float] | None
+            The result of the action method.
+        """
+        assert instruct.device == self
+        return self.act(action_name=instruct.action_name, action_parameters=instruct.action_parameters, actor_type=actor_type)
 
 
 class Instruction(Individual):
     """
-    an instruction sent to a device for an action
+    This class defines an instruction sent to a device for an action.
+
+    Parameters
+    ----------
+    device : Device
+        The device to send the instruction to.
+    action_parameters : dict
+        The parameters for the action method of the device.
+    action_name : str
+        The name of the action method of the device.
+    description : str
+        The description of this instruction.
+
+    Note
+    ----
 
     instruction:
     - an instruction is sent to and received by one and only one `Device` instance (the `actor`) instantly
@@ -150,37 +205,95 @@ class Instruction(Individual):
 
 
 class Lab(BaseModel):
+    """
+    A `Lab` is a collection of `LabObject` instances and operations on them.
+
+    Parameters
+    ----------
+    dict_object : dict[str, LabObject | Device]
+        A dictionary of all `LabObject` instances in this `Lab`.
+    dict_instruction : dict[str, Instruction]
+        A dictionary of all `Instruction` instances in this `Lab`.
+    """
+
     dict_instruction: dict[str, Instruction] = dict()
     dict_object: dict[str, LabObject | Device] = dict()
 
     def __getitem__(self, identifier: str):
+        """Getting item by key from dict_object."""
         return self.dict_object[identifier]
 
     def __setitem__(self, key, value):
+        """Setting an item with key and value pair to dict_object."""
         raise NotImplementedError
 
-    def act_by_instruction(self, i: Instruction, actor_type: DEVICE_ACTION_METHOD_ACTOR_TYPE):
-        actor = self.dict_object[i.device.identifier]  # make sure we are working on the same device
+    def act_by_instruction(self,
+                           instruct: Instruction,
+                           actor_type: DEVICE_ACTION_METHOD_ACTOR_TYPE):
+        """Perform action with an instruction.
+
+        Parameters
+        ----------
+        instruct : Instruction
+            The instruction to perform.
+        actor_type : DEVICE_ACTION_METHOD_ACTOR_TYPE
+            The type of actor method to call, one of 'pre', 'post', 'proj'.
+
+        Returns
+        -------
+        tuple[list[LabObject], float] | None
+            The result of the action method.
+        """
+        # make sure we are working on the same device
+        actor = self.dict_object[instruct.device.identifier]
         assert isinstance(actor, Device)
-        return actor.act_by_instruction(i, actor_type=actor_type)
+        return actor.act_by_instruction(instruct, actor_type=actor_type)
 
-    def add_instruction(self, i: Instruction):
-        assert i.identifier not in self.dict_instruction
-        self.dict_instruction[i.identifier] = i
+    def add_instruction(self, instruct: Instruction):
+        """Add an instruction to the lab.
 
-    def remove_instruction(self, i: Instruction | str):
-        if isinstance(i, str):
-            assert i in self.dict_instruction
-            self.dict_instruction.pop(i)
+        Parameters
+        ----------
+        instruct : Instruction
+            The instruction to add.
+        """
+        assert instruct.identifier not in self.dict_instruction
+        self.dict_instruction[instruct.identifier] = instruct
+
+    def remove_instruction(self, instruct: Instruction | str):
+        """Remove an instruction from the lab.
+
+        Parameters
+        ----------
+        instruct : Instruction | str
+            The instruction to remove.
+        """
+        if isinstance(instruct, str):
+            assert instruct in self.dict_instruction
+            self.dict_instruction.pop(instruct)
         else:
-            assert i.identifier in self.dict_instruction
-            self.dict_instruction.pop(i.identifier)
+            assert instruct.identifier in self.dict_instruction
+            self.dict_instruction.pop(instruct.identifier)
 
     def add_object(self, d: LabObject | Device):
+        """Add a `LabObject` or `Device` instance to the lab.
+
+        Parameters
+        ----------
+        d : LabObject | Device
+            The `LabObject` or `Device` instance to add.
+        """
         assert d.identifier not in self.dict_object
         self.dict_object[d.identifier] = d
 
     def remove_object(self, d: LabObject | Device | str):
+        """Remove a `LabObject` or `Device` instance from the lab.
+
+        Parameters
+        ----------
+        d : LabObject | Device | str
+            The `LabObject` or `Device` instance to remove.
+        """
         if isinstance(d, str):
             assert d in self.dict_object
             self.dict_object.pop(d)
@@ -190,19 +303,51 @@ class Lab(BaseModel):
 
     @property
     def state(self) -> dict[str, dict[str, Any]]:
+        """Get the state of the lab.
+
+        Returns
+        -------
+        dict[str, dict[str, Any]]
+            The state of the lab as a dictionary where the keys are the identifiers of the
+            `LabObject` instances and the values are the states of the `LabObject` instances.
+        """
         return {d.identifier: d.state for d in self.dict_object.values()}
 
     def dict_object_by_class(self, object_class: Type):
+        """Get a dictionary of `LabObject` instances of a certain class.
+
+        Parameters
+        ----------
+        object_class : Type
+            The class of the `LabObject` instances to get.
+
+        Returns
+        -------
+        dict[str, LabObject]
+            A dictionary of `LabObject` instances of the specified class.
+        """
         return {k: v for k, v in self.dict_object.items() if v.__class__ == object_class}
 
     def __repr__(self):
+        """Get a string representation of the lab, including the `LabObject` and `Device` and
+        the corresponding states."""
         return "\n".join([f"{obj.identifier}: {obj.state}" for obj in self.dict_object.values()])
 
     def __str__(self):
+        """Get a string representation of the lab, including the `LabObject` and `Device` and the
+        corresponding states."""
         return self.__repr__()
 
     @property
     def instruction_graph(self) -> drawio_diagram:
+        """Get a diagram of the instruction graph.
+
+        Returns
+        -------
+        drawio_diagram
+            The instruction graph.
+
+        """
         diagram = drawio_diagram()
         diagram.add_diagram("Page-1")
 
